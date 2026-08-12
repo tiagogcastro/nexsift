767475708eb2384dca3841748019904cb75da118
tiagogcastro <tiagogcastro1@gmail.com>
2026-08-12 10:47:03 -0300

    chore: remove unused packages, seed scripts, and bootstrap content

--- iac/terraform/ssm.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ssm_parameter" "publish_token" {
-  name             = "/nexsift/${var.environment}/publish-token"
-  type             = "SecureString"
-  value_wo         = var.publish_token
-  value_wo_version = var.publish_token_version
-
-  tags = local.common_tags
-}
