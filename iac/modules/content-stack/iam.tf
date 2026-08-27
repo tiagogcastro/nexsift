2d36490f11b9134e13e8600f220188db06d8bd62
tiagogcastro <tiagogcastro1@gmail.com>
2026-08-27 10:44:21 -0300

    fix(editorial): allow deleting published content

--- iac/modules/content-stack/iam.tf
+++ iac/modules/content-stack/iam.tf
@@ -23,6 +23,7 @@ data "aws_iam_policy_document" "publish" {
     actions = [
       "s3:GetObject",
       "s3:PutObject",
+      "s3:DeleteObject",
       "s3:ListBucket",
     ]
     resources = [
